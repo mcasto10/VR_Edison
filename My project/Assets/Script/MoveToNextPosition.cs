cfb33e9790c27bab5af13ec6ffb591e10d8afac9
Denise Tabilas <tabilas.denise@gmail.com>
2023-01-31 19:03:11 -0800

    testing teleporting

---
+++ My project/Assets/Script/MoveToNextPosition.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToNextPosition : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ToEquipment()
+    {
+        transform.position = new Vector3(38.5, -6.824, -17.4);
+    }
+
+}
