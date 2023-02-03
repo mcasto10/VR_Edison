9cb93bd98568779cadc53e4a23daca132bb6e319
Denise Tabilas <tabilas.denise@gmail.com>
2023-02-03 13:32:15 -0800

    l

--- My project/Assets/Script/MoveToNextPosition.cs
+++ My project/Assets/Script/MoveToNextPosition.cs
@@ -5,18 +5,21 @@ using UnityEngine.UI; // for button use
 
 public class MoveToNextPosition : MonoBehaviour
 {
-     public Button button;
+     public GameObject player;
 
     // Start is called before the first frame update
     void Start()
     {
-          button.onClick.AddListener(Teleport);
+          //button.onClick.AddListener(Teleport);
     }
 
 
-    public void Teleport()
+    public void Teleport(GameObject NextLocation)
     {
-        transform.position = new Vector3(38.5F, -6.824F, -17.4F);
-    }
+          Transform pos = NextLocation.transform;
+          //transform.position = new Vector3(38.5F, -6.824F, -17.4F);
+          
+          player.transform.position = pos.position;
+     }
 
 }
