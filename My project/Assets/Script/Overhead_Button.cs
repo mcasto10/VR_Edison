c7387d7daf7d4d15c23c737675b59f6c843b286e
Denise Tabilas <tabilas.denise@gmail.com>
2023-02-03 13:58:34 -0800

    fix light

--- My project/Assets/Script/Overhead_Button.cs
+++ My project/Assets/Script/Overhead_Button.cs
@@ -6,7 +6,6 @@ using UnityEngine.SceneManagement;
 public class Overhead_Button : MonoBehaviour {
 
     public void goToNextScene() {
-        Debug.Log("next scene");
         SceneManager.LoadScene("Overhead");
           
         
