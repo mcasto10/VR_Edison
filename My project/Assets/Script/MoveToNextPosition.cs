776a9818976d62c049f40c6d1d28a4d0914623d9
Denise Tabilas <tabilas.denise@gmail.com>
2023-02-06 23:05:21 -0800

    edited script, added documentation

--- My project/Assets/Script/MoveToNextPosition.cs
+++ My project/Assets/Script/MoveToNextPosition.cs
@@ -5,12 +5,24 @@ using UnityEngine.UI; // for button use
 
 public class MoveToNextPosition : MonoBehaviour
 {
-     public GameObject player;
+     public GameObject player; // gain access to the player's position
 
     public void Teleport(GameObject NextLocation)
     {
-          Transform pos = NextLocation.transform;
-          player.transform.position = pos.position;
-     }
+          Transform pos = NextLocation.transform; // get the position of the next location
+          player.transform.position = pos.position; // change the position of the player to the next location
+    }
+
+    // hard code teleport position template
+    // must create a different method/script
+    // if you want to teleport to different positions
+    public void Teleport()
+    {
+          // i just put 100, 100, 100
+          float x_pos = 100f;
+          float y_pos = 100f;
+          float z_pos = 100f;
+          player.transform.position = new Vector3(x_pos, y_pos, z_pos); // change position to the new Vector3 object
+    }
 
 }
