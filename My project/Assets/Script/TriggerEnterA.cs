de9a5d71670f2fc6d9ec76366504443c456013ca
mcasto10 <castorenamartin1234@gmail.com>
2023-03-14 19:40:52 -0700

    Updated positions

--- My project/Assets/Script/TriggerEnterA.cs
+++ My project/Assets/Script/TriggerEnterA.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TriggerEnter : MonoBehaviour
+public class TriggerEnterA : MonoBehaviour
 {
 
     // public OnTrigger trigger; 
