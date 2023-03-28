52dfe0eb7b0973ff40f455de20a1d3b762e6c1a0
mcasto10 <castorenamartin1234@gmail.com>
2023-03-27 21:17:47 -0700

    update

--- My project/Assets/Script/PoleMasterTrigger.cs
+++ My project/Assets/Script/PoleMasterTrigger.cs
@@ -1,25 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
 
 
-public class PoleMasterTrigger : MonoBehaviour
-{
+// public class PoleMasterTrigger : MonoBehaviour
+// {
 
-    public GameObject PoleMaster;
-    public GameObject Pole_Master_With_Pivot;
-//      public void sOnTriggerEnter(Collider col) {
-//         Debug.Log("Hello");
-//      if(col.GetComponent<Collider>().name == "PoleMaster") {
+//     public GameObject PoleMaster;
+//     public GameObject Pole_Master_With_Pivot;
+// //      public void sOnTriggerEnter(Collider col) {
+// //         Debug.Log("Hello");
+// //      if(col.GetComponent<Collider>().name == "PoleMaster") {
+// //         Pole_Master_With_Pivot.SetActive(true);
+// //         PoleMaster.SetActive(false);
+// //      }
+// //  }
+
+//  public void sOnTriggerEnter() {
 //         Pole_Master_With_Pivot.SetActive(true);
 //         PoleMaster.SetActive(false);
-//      }
-//  }
-
- public void sOnTriggerEnter() {
-        Pole_Master_With_Pivot.SetActive(true);
-        PoleMaster.SetActive(false);
      
- }
-}
+//  }
+// }
