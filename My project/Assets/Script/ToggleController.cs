3fe504e8ff93785c03c3dc6bb2be4fab1add155e
mcasto10 <castorenamartin1234@gmail.com>
2023-04-17 14:16:26 -0700

    Menu updates

--- My project/Assets/Script/ToggleController.cs
+++ My project/Assets/Script/ToggleController.cs
@@ -2,10 +2,121 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class ToggleController : MonoBehaviour
 {
-    public Toggle myToggle;
+   public GameObject player; // gain access to the player's position
+
+    public List<bool> boolList = new List<bool> {false, false, false, false};
+
+    //  void Start()
+    // {
+    //     Debug.Log(currentScore);
+    //         List<bool> boolList = currentScore.getTeleportZone();
+    //         ToggleBoolList(boolList);
+    
+    // }
+
+    void Update() {
+        boolList = new List<bool> {false, false, false, false};
+        // foreach(bool number in boolList) {
+        //     Debug.Log(number);
+
+        // }
+
+        //    Debug.Log("--------------------------");
+
+        Vector3 currentPosition = player.transform.position;
+        TeleportZone(currentPosition);
+        ToggleBoolList();
+
+    }
+
+      public List<bool> TeleportZone(Vector3 currentPosition) {
+          Vector3[] myVectors = new Vector3[4];
+          // Scene 2
+          myVectors[0] = new Vector3(2.962f, 0.63f, -3.27f);
+          // Scene 3
+          myVectors[1] = new Vector3(2.952f, 0.09f, -2.68f);
+          // scene 5
+          myVectors[2] = new Vector3(4.478518f, 2.26f, -20.00f);
+          // sccene 9
+          myVectors[3] = new Vector3(4.476227f, 0.09f, -2.65f);
+
+          float epsilon = 0.0001f;
+
+      for(int i = 0; i < myVectors.Length; i++){
+            if(Mathf.Abs(myVectors[i].x - currentPosition.x) < epsilon) {
+                for(int j = 0; j <= i; j++){
+                    if(j == 0){
+                        boolList.RemoveAt(0);
+                        boolList.Insert(0, true);
+                        // Debug.Log("Location 0");
+                        // setToggleScene2();
+
+                    }
+                    if(j == 1){
+                        boolList.RemoveAt(1);
+                        boolList.Insert(1, true);
+                        //  Debug.Log("Location 1");
+                        // setToggleScene3();
+                    }
+
+                    if(j == 2){
+                        boolList.RemoveAt(2);
+                        boolList.Insert(2, true);
+                        //  Debug.Log("Location 2");
+                        // setToggleScene5();
+                    }
+                    
+                    if(j == 3){
+                        boolList.RemoveAt(3);
+                        boolList.Insert(3, true);
+                        //  Debug.Log("Location 3");
+                        // setToggleScene9();
+                    }
+                }
+              }
+            }
+      return boolList; 
+    }
+
+    public void ToggleBoolList(){
+
+            for(int i = boolList.Count - 1; i >= 0; i--){
+                if(boolList[i] == true) {
+                    if(i == 3){
+                        setToggleScene9();
+                        setToggleScene5();
+                        setToggleScene3();
+                        setToggleScene2();
+                    }
+                    
+                    if(i == 2){
+                        unactiveToggleScene9();
+                        setToggleScene5();
+                        setToggleScene3();
+                        setToggleScene2();
+                        
+                    }
+
+                    if(i == 1){
+                        unactiveToggleScene9();
+                        unactiveToggleScene5();
+                        setToggleScene3();
+                        setToggleScene2();
+                    }
+                        
+                    if(i == 0){
+                        unactiveToggleScene9();
+                        unactiveToggleScene5();
+                        unactiveToggleScene3();
+                        setToggleScene2();
+                    }
+                }
+        }
+    }
 
     public void ToggleValueChanged(Transform pos)
     {
@@ -14,104 +125,115 @@ public class ToggleController : MonoBehaviour
 
           Vector3[] myVectors = new Vector3[4];
           // Scene 2
-          myVectors[0] = new Vector3(2.96f, 0.63f, -3.27f);
+          myVectors[0] = new Vector3(2.962f, 0.63f, -3.27f);
           // Scene 3
-          myVectors[1] = new Vector3(4.48f, 0.09f, -2.68f);
+          myVectors[1] = new Vector3(2.952f, 0.09f, -2.68f);
           // scene 5
-          myVectors[2] = new Vector3(2.95f, 2.26f, -20.00f);
+          myVectors[2] = new Vector3(4.478518f, 2.26f, -20.00f);
           // sccene 9
-          myVectors[3] = new Vector3(4.48f, 0.09f, -2.65f);
+          myVectors[3] = new Vector3(4.476227f, 0.09f, -2.65f);
+
+          float epsilon = 0.0001f;
+
+        //   Debug.Log("Value 1: " + currentPosition.x.ToString("0.00000000"));
+
+        //   Type floatType = currentPosition.x.GetType();
+        //   int floatSize = sizeof(float);
+
+        //   Debug.Log("Type: " + floatType); // Outputs "Type: System.Single"
+        //   Debug.Log("Size: " + floatSize); // Outputs "Size: 4"
        
-        Debug.Log(myToggle.ToString());
+    //    Debug.Log("--------------------------");
+
+        // Debug.Log(myToggle.ToString());
            
         // checking to see what scene the user is currently in, then updating all those that are valid
         for(int i = 0; i < myVectors.Length; i++){
-            Debug.Log(currentPosition + " current position");
-            Debug.Log(myVectors[i].ToString() + " loop position " + i );
+
+            // Debug.Log("Value 2: " + myVectors[i].x.ToString("0.00000000"));
+            // Debug.Log(currentPosition.x + " current position " + i);
+            // Debug.Log(myVectors[i].x + " loop position " + i );
+
+        //     Type valueType = myVectors[i].x.GetType();
+        //     int valueSize = sizeof(float);
+
+        //       Debug.Log("Type: " + valueType); // Outputs "Type: System.Single"
+        //   Debug.Log("Size: " + valueSize); // Outputs "Size: 4"
+
+            // Debug.Log(myVectors[i].x + " values" + currentPosition.x + "Current");
+            // Debug.Log(Mathf.Abs(myVectors[i].x - currentPosition.x) < epsilon);
+            // Debug.Log(Mathf.Approximately(myVectors[i].x, currentPosition.x));
             // Debug.Log(new Vector3((float)currentPosition.x, (float)currentPosition.y, (float)currentPosition.z) == myVectors[i]);
             // Debug.Log(myVectors[i] == currentPosition);
             // Debug.Log(myVectors[i].Equals(currentPosition));
 
-
             // if (new Vector3((float)currentPosition.x, (float)currentPosition.y, (float)currentPosition.z) == myVectors[i] ) {
-            if(myVectors[i] == currentPosition){
+            if(Mathf.Abs(myVectors[i].x - currentPosition.x) < epsilon) {
                 // Debug.Log(myVectors[i].ToString());
-                for(int j = 0; j < i; j++){
+                for(int j = 0; j <= i; j++){
+                    Debug.Log(j);
                     if(j == 0){
-                        Debug.Log("Location 1");
+                        Debug.Log("Location 0");
                         setToggleScene2();
 
                     }
                     if(j == 1){
-                         Debug.Log("Location 2");
-                        setToggleScene3();
+                         Debug.Log("Location 1");
+                        // setToggleScene3();
                     }
 
                     if(j == 2){
-                         Debug.Log("Location 3");
-                        setToggleScene5();
+                         Debug.Log("Location 2");
+                        // setToggleScene5();
                     }
                     
                     if(j == 3){
-                         Debug.Log("Location 4");
-                        setToggleScene9();
+                         Debug.Log("Location 3");
+                        // setToggleScene9();
                     }
                 }
-
-        }
-            //   if(i == 0){
-            //         setToggleScene2();
-            //     }
-            //     if(i == 1){
-            //         setToggleScene3();
-            //     }
-            //     if(i == 2){
-            //         setToggleScene5();
-            //     }
-            //     if(i == 3){
-            //         setToggleScene9();
-            //     }
+            }
         }
 
     }
 
     public void setToggleScene2(){
-        Debug.Log("Enter 2");
-        if(myToggle.isOn = !myToggle.isOn){
-             myToggle.isOn = true;
-        }
-        else {
-            myToggle.isOn = !myToggle.isOn;
-        }
+        Toggle myToggle = GameObject.FindWithTag("SetSafetyTag").GetComponent<Toggle>();
+        myToggle.isOn = true;
+    }
+
+    public void unactiveToggleScene2(){
+        Toggle myToggle = GameObject.FindWithTag("SetSafetyTag").GetComponent<Toggle>();
+        myToggle.isOn = false;
     }
 
     public void setToggleScene3(){
-         Debug.Log("Enter 3");
-          if(myToggle.isOn = !myToggle.isOn){
-             myToggle.isOn = true;
-        }
-        else {
-            myToggle.isOn = !myToggle.isOn;
-        }
-        
+        Toggle myToggle = GameObject.FindWithTag("SetPoleMaster").GetComponent<Toggle>();
+        myToggle.isOn = true;
     }
-    public void setToggleScene5(){
-         Debug.Log("Enter 5");
-          if(myToggle.isOn = !myToggle.isOn){
-             myToggle.isOn = true;
-        }
-        else {
-            myToggle.isOn = !myToggle.isOn;
-        }
+
+    public void unactiveToggleScene3(){
+        Toggle myToggle = GameObject.FindWithTag("SetPoleMaster").GetComponent<Toggle>();
+        myToggle.isOn = true;  
+    }
+
+     public void setToggleScene5(){
+        Toggle myToggle = GameObject.FindWithTag("SetSmartNav").GetComponent<Toggle>();
+        myToggle.isOn = true;
+    }
+
+
+    public void unactiveToggleScene5(){
+        Toggle myToggle = GameObject.FindWithTag("SetSmartNav").GetComponent<Toggle>();
+        myToggle.isOn = false;
     }
 
     public void setToggleScene9(){
-         Debug.Log("Enter 9");
-          if(myToggle.isOn = !myToggle.isOn){
-             myToggle.isOn = true;
-        }
-        else {
-            myToggle.isOn = !myToggle.isOn;
-        }
+        Toggle myToggle = GameObject.FindWithTag("SetCheckSmartNav").GetComponent<Toggle>();
+        myToggle.isOn = true;
+    }
+    public void unactiveToggleScene9(){
+       Toggle myToggle = GameObject.FindWithTag("SetCheckSmartNav").GetComponent<Toggle>();
+       myToggle.isOn = false;
     }
 }
