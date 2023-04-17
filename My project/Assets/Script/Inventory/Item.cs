911d225f4310969115bd8033ec11d5ac35791550
Denise Tabilas <tabilas.denise@gmail.com>
2023-04-17 09:05:13 -0700

    updated more files, donezos

--- My project/Assets/Script/Inventory/Item.cs
+++ My project/Assets/Script/Inventory/Item.cs
@@ -1,26 +1,38 @@
 ﻿
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
-public class Item : MonoBehaviour
-{
-     public bool inSlot;
-     public Vector3 slotRotation = Vector3.zero;
-     public Slot currentSlot;
 
-     public void GrabFromSlot(GameObject obj)
+     public class Item : MonoBehaviour
      {
-          if (obj.GetComponent<Item>() == null) return;
-          if (obj.GetComponent<Item>().inSlot)
+          public bool inSlot;
+          public Vector3 slotRotation = Vector3.zero;
+          public Slot currentSlot;
+
+          public void GrabFromSlot()
           {
-               obj.GetComponentInParent<Slot>().ItemInSlot = null;
-               obj.transform.parent = null;
-               obj.GetComponent<Item>().inSlot = false;
-               obj.GetComponent<Item>().currentSlot.ResetColor();
-               obj.GetComponent<Item>().currentSlot = null;
+               //if (obj.GetComponent<Item>() == null) return;
+               //if (obj.GetComponent<Item>().inSlot)
+               //{
+               //     obj.GetComponentInParent<Slot>().ItemInSlot = null;
+               //     obj.transform.parent = null;
+               //     obj.GetComponent<Item>().inSlot = false;
+               //     obj.GetComponent<Item>().currentSlot.ResetColor();
+               //     obj.GetComponent<Item>().currentSlot = null;
+               //}
+               if (inSlot == true)
+               {
+                    currentSlot.ItemInSlot = null;
+                    gameObject.transform.parent = null;
+                    inSlot = false;
+                    currentSlot.ResetColor();
+                    currentSlot = null;
+                    gameObject.GetComponent<XRGrabInteractable>().SetKinematic(false);
+               }
+          }
+          public void Update()
+          {
+
           }
      }
-     public void Update()
-     {
 
-     }
-}
