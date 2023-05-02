f92c0e6c0c6e903d04c05d1f239fbdc7e6bb8252
mcasto10 <castorenamartin1234@gmail.com>
2023-05-02 14:41:31 -0700

    Updated Menu

--- My project/Assets/Script/Inventory/InventoryVR.cs
+++ My project/Assets/Script/Inventory/InventoryVR.cs
@@ -6,13 +6,17 @@ using UnityEngine.UI;
 public class InventoryVR : MonoBehaviour
 {
      public GameObject Inventory;
+     public GameObject MenuBar;
      public GameObject Anchor;
      bool UIActive;
+     bool UIMenuActive;
 
      private void Start()
      {
           Inventory.SetActive(false);
           UIActive = false;
+          MenuBar.SetActive(false);
+          UIMenuActive = false;
      }
 
      private void Update()
@@ -26,6 +30,17 @@ public class InventoryVR : MonoBehaviour
           {
                Inventory.transform.position = Anchor.transform.position;
                Inventory.transform.eulerAngles = new Vector3(Anchor.transform.eulerAngles.x + 15, Anchor.transform.eulerAngles.y, 0);
+          }
+
+           if (Input.GetButtonDown("XRI_Left_PrimaryButton"))
+          {
+               UIMenuActive = !UIMenuActive;
+               MenuBar.SetActive(UIMenuActive);
+          }
+          if (UIMenuActive)
+          {
+               MenuBar.transform.position = Anchor.transform.position;
+               MenuBar.transform.eulerAngles = new Vector3(Anchor.transform.eulerAngles.x + 15, Anchor.transform.eulerAngles.y, 0);
           }
      }
 }
